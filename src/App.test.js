a51412d28163c0920f833bf9b2ec34b6d9e28484
SamNicoll <sam.nicoll@verint.com>
2019-11-29 15:30:13 +0000

    tests... and they actually pass

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  let app;
+  let inputWrapper;
+
+  const changeInputValue = (value) => {
+    inputWrapper.simulate('change', {target: {value: value}});
+  };
+
+  const outputWrapper = () => {
+    return app.find('#output');
+  };
+
+  const outputValue = () => {
+    return outputWrapper().prop('value');
+  };
+
+  beforeEach(() => {
+    configure({ adapter: new Adapter() });
+    app = shallow(<App/>);
+    inputWrapper = app.find('#input');
+  });
