421492ba59439e79c7ff966e65ff601a9cf5e1e2
SamNicoll <sam.nicoll@verint.com>
2019-11-29 15:49:01 +0000

    ACTUALLY did some stuff

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import './App.css';
+import {TextField} from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Iain Plus Plus</h2>
-          <h4>we're at step 5</h4>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    calculate = (value) => {
+        let arr = value.split("");
+        let total = 0;
+        arr.forEach(element => {
+            if (element === "/") {
+                total = 10;
+                return;
+            }
+            total += element === "-" ? 0 : parseInt(element)
+        });
+        return total;
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
+        };
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange = (event) => {
+        this.setState({
+            value: this.calculate(event.target.value)
+        });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">FizzBuzz</header>
+                <TextField id="input" variant="filled" label="Input" onChange={this.onChange}/>
+                <TextField id="output" variant="outlined" readOnly={true} value={this.state.value}/>
+            </div>
+        )
+    };
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {configure, shallow} from 'enzyme';
 import App from './App';
 import Adapter from 'enzyme-adapter-react-16';
 
@@ -20,7 +20,43 @@ describe('App', () => {
   };
 
   beforeEach(() => {
-    configure({ adapter: new Adapter() });
+    configure({adapter: new Adapter()});
     app = shallow(<App/>);
     inputWrapper = app.find('#input');
   });
+
+  it('should show header', () => {
+    expect(app.find('header').text()).toBe('FizzBuzz');
+  });
+
+  describe('bowling score calculate', function () {
+    it('should calculate score as zero for all misses', function () {
+      changeInputValue('--------------------');
+      expect(outputValue()).toEqual(0);
+    });
+
+    it('should calculate score as 1 for a single hit,  all misses ', function () {
+      changeInputValue('1-------------------');
+      expect(outputValue()).toEqual(1);
+    });
+
+    it('should calculate score as 2 for a two pin hit, then all misses ', function () {
+      changeInputValue('2-------------------');
+      expect(outputValue()).toEqual(2);
+    });
+
+    it('should calculate score for two single hits, then all misses ', function () {
+      changeInputValue('11------------------');
+      expect(outputValue()).toEqual(2);
+    });
+
+    it('should calculate score for spare, then all misses ', function () {
+      changeInputValue('-/-------------------');
+      expect(outputValue()).toEqual(10);
+    });
+    // it('should calculate score of 11 for a hit then spare , then all misses ', function () {
+    //   changeInputValue('-1-/----------------');
+    //   expect(outputValue()).toEqual(11);
+    // });
+  });
+});
